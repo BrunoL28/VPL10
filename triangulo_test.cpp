3f150ae3bcf459c25dbbf469ccdeff4a0ccb3a1b
Bruno Lima <brunolima280103@gmail.com>
2022-11-26 23:15:22 -0300

    Definindo escopos e estrutura do código

---
+++ triangulo_test.cpp
@@ -0,0 +1,46 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest.h"
+#include "triangulo.hpp"
+
+TEST_CASE("Testando a primeira função, getPerimeter: "){
+
+    SUB_CASE("Caso positivo: "){
+
+
+    }
+
+    SUB_CASE("Caso negativo: "){
+
+
+    }
+
+}
+
+TEST_CASE("Testando a segunda função, getArea: "){
+
+    SUB_CASE("Caso positivo: "){
+
+
+    }
+
+    SUB_CASE("Caso negativo: "){
+
+
+    }
+
+}
+
+TEST_CASE("Testando a terceira função, getKind: "){
+
+    SUB_CASE("Caso positivo: "){
+
+
+    }
+
+    SUB_CASE("Caso negativo: "){
+
+        
+    }
+
+}
